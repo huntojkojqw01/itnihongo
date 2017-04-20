4509cedde535ac2bfa29904ebeecdd5ed894fb3e
huntojkojqw01 <hungnd.k58@gmail.com>
2017-04-21 02:10:24 +0700

    backup ok

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ApplicationRecord
   mount_uploader :image, PhotoUploader
-  belongs_to :album
+  belongs_to :album  
   has_many :likes, dependent: :destroy
   has_many :users, through: :likes
   has_many :comments, dependent: :destroy
