ae7f8a6a1ab99692515610c9f391c3575b7f8cd3
huntojkojqw01 <hungnd.k58@gmail.com>
2017-05-01 23:37:45 +0700

    fix for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { :host => 'your.websitedomain.com' }
+  config.action_mailer.default_url_options = { :host => 'kawaiipetto.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
     :address        => "smtp.sendgrid.net",
     :port           => "25",
